2045ad0376869bf01e7a45789ec4c4b958f12e55
DivyenAswar <divyenaswar19@gmail.com>
2025-12-29 15:37:36 +0000

    its js

---
+++ comparison.js
@@ -0,0 +1,28 @@
+//Comparison Data Types
+
+//1.number
+
+console.log(10 > 5)      // true
+console.log(10 === 10)  // true
+console.log(10 == "10" )// true but dangerous
+
+
+
+let num = 10;
+let str = "10";
+let bool = true;
+let n = null;
+let u;
+let arr1 = [1];
+let arr2 = [1];
+let obj1 = {};
+let obj2 = {};
+
+console.log(num == str);   
+console.log(num === str);   
+console.log(bool == 1);     
+console.log(n == u);        
+console.log(n >= 0);        
+console.log(arr1 == arr2);  
+console.log(obj1 === obj2); 
+
---
+++ type_conversion.js
@@ -0,0 +1,18 @@
+let number = 33
+
+//here is return number datatype 
+console.log(typeof number);
+console.log(typeof (number));
+
+let score = "33" 
+console.log(typeof score); // here its return string because number in ""
+
+
+let value = Number(score) // if you give  Number than its return number dt 
+console.log(typeof value);
+
+
+
+
+
+
